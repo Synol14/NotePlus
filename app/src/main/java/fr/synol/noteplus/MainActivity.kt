ea5ecf8ee514c4ee794bbe639ad9f3ef37bddde2
Synol_ <louisvaillant44@gmail.com>
2021-10-09 13:09:01 +0200

    First Commit - Starting First Fragment

--- app/src/main/java/fr/synol/noteplus/MainActivity.kt
+++ app/src/main/java/fr/synol/noteplus/MainActivity.kt
@@ -2,10 +2,17 @@ package fr.synol.noteplus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.synol.noteplus.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // inject HomeFragment in fragment_container
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
